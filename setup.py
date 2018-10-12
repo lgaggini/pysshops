e8317275d1856659b3fcb729e5997c43859ed9ec
Lorenzo Gaggini <lg@lgaggini.net>
2018-10-12 09:59:16 +0200

    0.2.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as file:
 
 setup(
     name='pysshops',
-    version=' 0.2.1',
+    version=' 0.2.2',
     author='Lorenzo Gaggini',
     author_email='lg@lgaggini.net',
     packages=['pysshops'],
